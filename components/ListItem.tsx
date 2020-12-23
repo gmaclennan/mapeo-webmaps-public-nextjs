afe500d33cc0b48cc5b5382241cdf192fa3e9b63
Gregor MacLennan <gmaclennan@digital-democracy.org>
2020-12-23 10:31:34 +0000

    Fix optimized images in left column

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,12 +1,23 @@
 import format from 'date-fns/format'
 import Image from 'next/image'
 
+const IMAGE_SIZE = [96, 72]
+
 export function ListItem({ title, date, image }) {
   const formattedDate = format(new Date(date.split('T')[0]), 'do MMM yyyy')
   return (
     <button className="flex border-t border-gray-300 border-solid w-full text-left py-2.5 px-4">
-      <div className="w-20 h-16 relative bg-pink-500 flex-0 mr-3 overflow-hidden">
-        <Image width={80} height={64} src={image} priority />
+      <div
+        className="relative flex-0 mr-3 overflow-hidden"
+        style={{ width: IMAGE_SIZE[0], height: IMAGE_SIZE[1] }}
+      >
+        <Image
+          width={IMAGE_SIZE[0]}
+          height={IMAGE_SIZE[1]}
+          objectFit="cover"
+          src={image}
+          priority
+        />
       </div>
       <div className="flex-1">
         <h2 className="font-bold leading-tight xl:text-lg xl:leading-snug mb-1 xl:mb-0">
