0d63f822df560508804e63a24381874e273730cd
Gregor MacLennan <gmaclennan@digital-democracy.org>
2021-01-04 15:19:03 +0000

    Remove PWA & service worker

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
 const withPreact = require('next-plugin-preact')
 
-const config = withPreact(
-  withPWA({
-    pwa: {
-      dest: 'public',
-      runtimeCaching,
-    },
-    images: {
-      domains: ['firebasestorage.googleapis.com'],
-    },
-  })
-)
+const config = withPreact({
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+})
 
 module.exports = config
